5bca7ea3859f390e7fc554f2ee4dcc97de89b3ce
vkryl <6242627+vkryl@users.noreply.github.com>
2021-08-20 01:56:48 +0300

    Fixed loading messages when scrolling chat after pressing "Beginning" in chat message search

--- app/src/main/java/org/drinkmore/Tracer.java
+++ app/src/main/java/org/drinkmore/Tracer.java
@@ -28,25 +28,25 @@ public class Tracer {
     return String.format(Locale.US, PREFIX, Client.getClientCount(), System.currentTimeMillis(), message);
   }
 
-  private static class ClientException_1422 extends RuntimeException {
-    private ClientException_1422 (String message) {
+  private static class ClientException_1423 extends RuntimeException {
+    private ClientException_1423 (String message) {
       super(format(message));
     }
   }
 
-  private static class DatabaseError extends ClientException_1422 {
+  private static class DatabaseError extends ClientException_1423 {
     private DatabaseError (String message) {
       super(message + ", versionCode: " + BuildConfig.VERSION_CODE);
     }
   }
 
-  private static class TdlibLaunchError extends ClientException_1422 {
+  private static class TdlibLaunchError extends ClientException_1423 {
     private TdlibLaunchError (String message) {
       super(message);
     }
   }
 
-  private static class TdlibLostPromiseError extends ClientException_1422 {
+  private static class TdlibLostPromiseError extends ClientException_1423 {
     private TdlibLostPromiseError (String message) {
       super(message);
     }
@@ -60,8 +60,8 @@ public class Tracer {
       newElements[0] = new StackTraceElement("org.drinkmore.Tracer", "throwError", "Tracer.java", 49);
       throwable.setStackTrace(newElements);
     }
-    if (throwable instanceof ClientException_1422)
-      throw (ClientException_1422) throwable;
+    if (throwable instanceof ClientException_1423)
+      throw (ClientException_1423) throwable;
     RuntimeException exception = new RuntimeException(format(throwable.getClass().getSimpleName() + ": " + throwable.getMessage()), throwable.getCause());
     exception.setStackTrace(throwable.getStackTrace());
     throw exception;
@@ -146,11 +146,11 @@ public class Tracer {
       // message only
 
       private void throwTestError (Throwable error) {
-        throw new ClientException_1422(error.getMessage());
+        throw new ClientException_1423(error.getMessage());
       }
 
       private void throwAssertionError (Throwable error) {
-        throw new ClientException_1422(error.getMessage());
+        throw new ClientException_1423(error.getMessage());
       }
 
       // Full trace
