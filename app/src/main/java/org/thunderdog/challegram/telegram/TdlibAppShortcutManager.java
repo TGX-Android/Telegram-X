e9b7eba09140bf0103f5262c60d5cee11d95b05c
vkryl <6242627+vkryl@users.noreply.github.com>
2021-09-07 00:48:15 +0300

    fixed tab size

--- app/src/main/java/org/thunderdog/challegram/telegram/TdlibAppShortcutManager.java
+++ app/src/main/java/org/thunderdog/challegram/telegram/TdlibAppShortcutManager.java
@@ -25,140 +25,140 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TdlibAppShortcutManager {
-    private final Tdlib tdlib;
+  private final Tdlib tdlib;
 
-    private long[] chatIds;
-    private Bitmap[] userAvatars;
-    private int avatarLoadIndex;
+  private long[] chatIds;
+  private Bitmap[] userAvatars;
+  private int avatarLoadIndex;
 
-    public TdlibAppShortcutManager (Tdlib tdlib) {
-        this.tdlib = tdlib;
+  public TdlibAppShortcutManager (Tdlib tdlib) {
+    this.tdlib = tdlib;
+  }
+
+  public void requestAndPublish () {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
+      return;
     }
 
-    public void requestAndPublish () {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
-            return;
+    if (chatIds != null && userAvatars != null) {
+      tdlib.runOnTdlibThread(() -> {
+        avatarLoadIndex = 0;
+        loadAvatar(chatIds[avatarLoadIndex]);
+      });
+    } else {
+      tdlib.client().send(new TdApi.GetTopChats(new TdApi.TopChatCategoryUsers(), 5), result -> {
+        if (result.getConstructor() != TdApi.Chats.CONSTRUCTOR) {
+          return;
         }
 
-        if (chatIds != null && userAvatars != null) {
-            tdlib.runOnTdlibThread(() -> {
-                avatarLoadIndex = 0;
-                loadAvatar(chatIds[avatarLoadIndex]);
-            });
-        } else {
-            tdlib.client().send(new TdApi.GetTopChats(new TdApi.TopChatCategoryUsers(), 5), result -> {
-                if (result.getConstructor() != TdApi.Chats.CONSTRUCTOR) {
-                    return;
-                }
+        TdApi.Chats chats = (TdApi.Chats) result;
 
-                TdApi.Chats chats = (TdApi.Chats) result;
+        this.chatIds = ((TdApi.Chats) result).chatIds;
+        this.userAvatars = new Bitmap[chats.chatIds.length];
+        this.avatarLoadIndex = 0;
 
-                this.chatIds = ((TdApi.Chats) result).chatIds;
-                this.userAvatars = new Bitmap[chats.chatIds.length];
-                this.avatarLoadIndex = 0;
-
-                loadAvatar(this.chatIds[avatarLoadIndex]);
-            });
-        }
+        loadAvatar(this.chatIds[avatarLoadIndex]);
+      });
     }
+  }
 
-    public void clear () {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
-            return;
-        }
-        android.content.pm.ShortcutManager sm = (android.content.pm.ShortcutManager) UI.getAppContext().getSystemService(Context.SHORTCUT_SERVICE);
-        sm.removeAllDynamicShortcuts();
+  public void clear () {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
+      return;
     }
+    android.content.pm.ShortcutManager sm = (android.content.pm.ShortcutManager) UI.getAppContext().getSystemService(Context.SHORTCUT_SERVICE);
+    sm.removeAllDynamicShortcuts();
+  }
 
-    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
-    private void loadAvatar (long currentChatId) {
-        Runnable onEnd = () -> {
-            if (++avatarLoadIndex == chatIds.length) {
-                onLoadFinished();
-            } else {
-                loadAvatar(chatIds[avatarLoadIndex]);
-            }
-        };
+  @RequiresApi(api = Build.VERSION_CODES.N_MR1)
+  private void loadAvatar (long currentChatId) {
+    Runnable onEnd = () -> {
+      if (++avatarLoadIndex == chatIds.length) {
+        onLoadFinished();
+      } else {
+        loadAvatar(chatIds[avatarLoadIndex]);
+      }
+    };
 
-        TdApi.ChatPhotoInfo photo = tdlib.chatPhoto(currentChatId);
-        if (photo != null) {
-            ImageFile file = new ImageFile(tdlib, photo.small, null);
-            file.setSize(150);
-            ImageLoader.instance().loadFile(file, (success, bitmap) -> {
-                if (success) {
-                    userAvatars[avatarLoadIndex] = circleCropBitmap(bitmap);
-                }
-
-                onEnd.run();
-            });
-        } else {
-            onEnd.run();
-        }
-    }
-
-    private static String getShortcutId (long accountId, long localChatId) {
-        return accountId + "_chat" + localChatId;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
-    private void onLoadFinished () {
-        ArrayList<android.content.pm.ShortcutInfo> shortcuts = new ArrayList<>();
-
-        int index = 0;
-        for (long chatId : chatIds) {
-            long localChatId = tdlib.settings().getLocalChatId(chatId);
-
-            android.content.pm.ShortcutInfo.Builder shortcutInfo = new android.content.pm.ShortcutInfo.Builder(UI.getAppContext(), getShortcutId(tdlib.accountId(), localChatId));
-
-            Intent intent = Intents.valueOfLocalChatId(tdlib.id(), localChatId, 0, true);
-            shortcutInfo.setShortLabel(tdlib.chatTitleShort(chatId));
-            shortcutInfo.setLongLabel(tdlib.chatTitle(chatId));
-            shortcutInfo.setIntent(intent);
-            shortcutInfo.setActivity(new ComponentName(UI.getAppContext(), MainActivity.class));
-
-            Set<String> categories = new HashSet<>();
-            categories.add(android.content.pm.ShortcutInfo.SHORTCUT_CATEGORY_CONVERSATION);
-
-            Bitmap bitmap;
-            if (userAvatars[index] != null) {
-                bitmap = userAvatars[index];
-            } else {
-                final int size = 150;
-                AvatarPlaceholder placeholder = tdlib.chatPlaceholder(chatId, tdlib.chat(chatId), true, size / 2f, null);
-                bitmap = renderLetterBitmap(placeholder, size);
-            }
-            shortcutInfo.setIcon(android.graphics.drawable.Icon.createWithBitmap(bitmap));
-            shortcutInfo.setCategories(categories);
-            shortcuts.add(shortcutInfo.build());
-            index++;
+    TdApi.ChatPhotoInfo photo = tdlib.chatPhoto(currentChatId);
+    if (photo != null) {
+      ImageFile file = new ImageFile(tdlib, photo.small, null);
+      file.setSize(150);
+      ImageLoader.instance().loadFile(file, (success, bitmap) -> {
+        if (success) {
+          userAvatars[avatarLoadIndex] = circleCropBitmap(bitmap);
         }
 
-        android.content.pm.ShortcutManager sm = (android.content.pm.ShortcutManager) UI.getAppContext().getSystemService(Context.SHORTCUT_SERVICE);
-        sm.setDynamicShortcuts(shortcuts);
+        onEnd.run();
+      });
+    } else {
+      onEnd.run();
+    }
+  }
+
+  private static String getShortcutId (long accountId, long localChatId) {
+    return accountId + "_chat" + localChatId;
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.N_MR1)
+  private void onLoadFinished () {
+    ArrayList<android.content.pm.ShortcutInfo> shortcuts = new ArrayList<>();
+
+    int index = 0;
+    for (long chatId : chatIds) {
+      long localChatId = tdlib.settings().getLocalChatId(chatId);
+
+      android.content.pm.ShortcutInfo.Builder shortcutInfo = new android.content.pm.ShortcutInfo.Builder(UI.getAppContext(), getShortcutId(tdlib.accountId(), localChatId));
+
+      Intent intent = Intents.valueOfLocalChatId(tdlib.id(), localChatId, 0, true);
+      shortcutInfo.setShortLabel(tdlib.chatTitleShort(chatId));
+      shortcutInfo.setLongLabel(tdlib.chatTitle(chatId));
+      shortcutInfo.setIntent(intent);
+      shortcutInfo.setActivity(new ComponentName(UI.getAppContext(), MainActivity.class));
+
+      Set<String> categories = new HashSet<>();
+      categories.add(android.content.pm.ShortcutInfo.SHORTCUT_CATEGORY_CONVERSATION);
+
+      Bitmap bitmap;
+      if (userAvatars[index] != null) {
+        bitmap = userAvatars[index];
+      } else {
+        final int size = 150;
+        AvatarPlaceholder placeholder = tdlib.chatPlaceholder(chatId, tdlib.chat(chatId), true, size / 2f, null);
+        bitmap = renderLetterBitmap(placeholder, size);
+      }
+      shortcutInfo.setIcon(android.graphics.drawable.Icon.createWithBitmap(bitmap));
+      shortcutInfo.setCategories(categories);
+      shortcuts.add(shortcutInfo.build());
+      index++;
     }
 
-    private static Bitmap renderLetterBitmap (AvatarPlaceholder placeholder, int size) {
-        Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        float radius = size / 2f;
-        placeholder.draw(canvas, radius, radius);
-        return bitmap;
-    }
+    android.content.pm.ShortcutManager sm = (android.content.pm.ShortcutManager) UI.getAppContext().getSystemService(Context.SHORTCUT_SERVICE);
+    sm.setDynamicShortcuts(shortcuts);
+  }
 
-    // The following part is wisely taken from Coil-KT
+  private static Bitmap renderLetterBitmap (AvatarPlaceholder placeholder, int size) {
+    Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap);
+    float radius = size / 2f;
+    placeholder.draw(canvas, radius, radius);
+    return bitmap;
+  }
 
-    private static Bitmap circleCropBitmap (Bitmap adaptiveIconBitmap) {
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
+  // The following part is wisely taken from Coil-KT
 
-        int minSize = Math.min(adaptiveIconBitmap.getWidth(), adaptiveIconBitmap.getHeight());
-        float radius = minSize / 2f;
+  private static Bitmap circleCropBitmap (Bitmap adaptiveIconBitmap) {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
 
-        Bitmap newBitmap = Bitmap.createBitmap(adaptiveIconBitmap.getWidth(), adaptiveIconBitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(newBitmap);
-        canvas.drawCircle(radius, radius, radius, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(adaptiveIconBitmap, radius - adaptiveIconBitmap.getWidth() / 2f, radius - adaptiveIconBitmap.getHeight() / 2f, paint);
+    int minSize = Math.min(adaptiveIconBitmap.getWidth(), adaptiveIconBitmap.getHeight());
+    float radius = minSize / 2f;
 
-        return newBitmap;
-    }
+    Bitmap newBitmap = Bitmap.createBitmap(adaptiveIconBitmap.getWidth(), adaptiveIconBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(newBitmap);
+    canvas.drawCircle(radius, radius, radius, paint);
+    paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+    canvas.drawBitmap(adaptiveIconBitmap, radius - adaptiveIconBitmap.getWidth() / 2f, radius - adaptiveIconBitmap.getHeight() / 2f, paint);
+
+    return newBitmap;
+  }
 }
