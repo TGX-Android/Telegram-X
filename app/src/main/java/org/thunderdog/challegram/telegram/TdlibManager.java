c54ade262fcfbbcdcf0d94d30167b90be606235b
vkryl <6242627+vkryl@users.noreply.github.com>
2021-10-29 18:57:38 +0300

    Improved titles / subtitles on Devices screen

--- app/src/main/java/org/thunderdog/challegram/telegram/TdlibManager.java
+++ app/src/main/java/org/thunderdog/challegram/telegram/TdlibManager.java
@@ -43,6 +43,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
