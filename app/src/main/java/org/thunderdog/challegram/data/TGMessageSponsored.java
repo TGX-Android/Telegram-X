9396cf1e3d4d0ba1da3c78509520e7bb20eb1cbe
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2021-11-08 18:16:27 +0100

    Randomize debug sponsored messages generation
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- app/src/main/java/org/thunderdog/challegram/data/TGMessageSponsored.java
+++ app/src/main/java/org/thunderdog/challegram/data/TGMessageSponsored.java
@@ -4,6 +4,8 @@ import org.drinkless.td.libcore.telegram.TdApi;
 import org.thunderdog.challegram.component.chat.MessagesManager;
 import org.thunderdog.challegram.telegram.Tdlib;
 
+import me.vkryl.core.MathUtils;
+
 public class TGMessageSponsored {
   public static TGMessage sponsoredToTgx (MessagesManager manager, long chatId, int date, TdApi.SponsoredMessage sMsg) {
     return new TGMessageText(manager, sponsoredToTd(chatId, date, sMsg), (TdApi.MessageText) sMsg.content);
@@ -30,9 +32,22 @@ public class TGMessageSponsored {
   }
 
   public static TdApi.SponsoredMessage generateSponsoredMessage (Tdlib tdlib) {
+    return (MathUtils.random(0, 1) == 1) ? generateBotSponsoredMessage(tdlib) : generateUserSponsoredMessage(tdlib);
+  }
+
+  private static TdApi.SponsoredMessage generateUserSponsoredMessage (Tdlib tdlib) {
     TdApi.SponsoredMessage msg = new TdApi.SponsoredMessage();
     msg.sponsorChatId = tdlib.myUserId();
     msg.id = 1;
+    msg.content = new TdApi.MessageText(new TdApi.FormattedText("Test ad message", null), null);
+    return msg;
+  }
+
+  private static TdApi.SponsoredMessage generateBotSponsoredMessage (Tdlib tdlib) {
+    TdApi.SponsoredMessage msg = new TdApi.SponsoredMessage();
+    msg.sponsorChatId = 3593;
+    msg.startParameter = "testParam";
+    msg.id = 2;
     msg.content = new TdApi.MessageText(new TdApi.FormattedText("Test ad message", null), null);
     return msg;
   }
