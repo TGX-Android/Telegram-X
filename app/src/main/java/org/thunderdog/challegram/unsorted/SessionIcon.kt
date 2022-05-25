324dd9467bd586c5cf525d74fcec59370b308463
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2022-05-25 12:58:05 +0200

    Rewrite SessionIcon.kt to use TDLib’s SessionType
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- app/src/main/java/org/thunderdog/challegram/unsorted/SessionIcon.kt
+++ app/src/main/java/org/thunderdog/challegram/unsorted/SessionIcon.kt
@@ -1,37 +1,26 @@
 package org.thunderdog.challegram.unsorted
 
+import androidx.annotation.DrawableRes
 import org.drinkless.td.libcore.telegram.TdApi
 import org.thunderdog.challegram.R
 
-/*
-  Android X:
-- apiId check
-
-Android, Windows, Linux, Ubuntu:
-- platform field match
-
-iMac, iPhone, iPad, MacBook, iWatch:
-- platform + deviceModel field match
-
-Chrome, Edge, Firefox, Safari:
-- platform or deviceModel (needs further researching)
-
-other cases - Other is used
-   */
-
-fun TdApi.Session.asIcon () = when {
-  apiId == 21724 -> R.drawable.baseline_device_android_x
-  containsModel("firefox") -> R.drawable.templarian_device_web_firefox
-  containsModel("opera") -> R.drawable.baseline_device_web_opera
-  containsModel("edg") -> R.drawable.templarian_device_web_edge // Chrome-based Edge names itself as "Edg", while EdgeHTML one is "Edge"
-  containsModel("chrome") -> R.drawable.templarian_device_web_chrome
-  containsModel("safari") -> R.drawable.templarian_device_web_safari
-  containsPlatform("android") -> R.drawable.baseline_device_android
-  containsPlatform("windows") -> R.drawable.baseline_device_windows
-  containsPlatform("mac") -> if (containsModel("macbook")) R.drawable.baseline_device_macbook else R.drawable.baseline_device_imac
-  containsPlatform("ios") -> if (containsModel("ipad")) R.drawable.baseline_device_ipad else R.drawable.baseline_device_iphone
-  else -> R.drawable.baseline_device_other
+@DrawableRes
+fun TdApi.Session.asIcon (): Int {
+  return if (apiId == 21724) {
+    R.drawable.baseline_device_android_x
+  } else when (type.constructor) {
+    TdApi.SessionTypeFirefox.CONSTRUCTOR -> R.drawable.templarian_device_web_firefox
+    TdApi.SessionTypeOpera.CONSTRUCTOR -> R.drawable.baseline_device_web_opera
+    TdApi.SessionTypeEdge.CONSTRUCTOR -> R.drawable.templarian_device_web_edge
+    TdApi.SessionTypeChrome.CONSTRUCTOR -> R.drawable.templarian_device_web_chrome
+    TdApi.SessionTypeSafari.CONSTRUCTOR -> R.drawable.templarian_device_web_safari
+    TdApi.SessionTypeAndroid.CONSTRUCTOR -> R.drawable.baseline_device_android
+    TdApi.SessionTypeWindows.CONSTRUCTOR -> R.drawable.baseline_device_windows
+    TdApi.SessionTypeMac.CONSTRUCTOR -> if (containsModel("macbook")) R.drawable.baseline_device_macbook else R.drawable.baseline_device_imac
+    TdApi.SessionTypeIphone.CONSTRUCTOR -> R.drawable.baseline_device_iphone
+    TdApi.SessionTypeIpad.CONSTRUCTOR -> R.drawable.baseline_device_ipad
+    else -> R.drawable.baseline_device_other
+  }
 }
 
 private fun TdApi.Session.containsModel (target: String) = deviceModel.contains(target, true)
-private fun TdApi.Session.containsPlatform (target: String) = platform.contains(target, true) || systemVersion.contains(target, true)
