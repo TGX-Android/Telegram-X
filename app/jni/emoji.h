6a3333561351963768b675c4823a71caba7c8840
vkryl <6242627+vkryl@users.noreply.github.com>
2022-06-11 22:49:33 +0300

    Update C/C++ copyright header template

--- app/jni/emoji.h
+++ app/jni/emoji.h
@@ -1,6 +1,17 @@
-//
-// Created by default on 8/1/17.
-//
+/*
+ * This file is a part of Telegram X
+ * Copyright © 2014-2022 (tgx-android@pm.me)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * File created on 01/08/2017
+ */
 
 #ifndef CHALLEGRAM_EMOJI_H
 #define CHALLEGRAM_EMOJI_H
--- app/jni/utils.cpp
+++ app/jni/utils.cpp
@@ -1,3 +1,16 @@
+/*
+ * This file is a part of Telegram X
+ * Copyright © 2014-2022 (tgx-android@pm.me)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "utils.h"
 #include <string>
 #include <jni_utils.h>
