cfc390c865d1230c28875c62d796ed56ce69236d
vkryl <6242627+vkryl@users.noreply.github.com>
2022-06-16 02:00:44 +0200

    Update Kotlin copyright header template

--- buildSrc/src/main/kotlin/Utils.kt
+++ buildSrc/src/main/kotlin/Utils.kt
@@ -1,4 +1,15 @@
-
+/*
+ * This file is a part of Telegram X
+ * Copyright © 2014-2022 (tgx-android@pm.me)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 import com.android.build.api.dsl.BaseFlavor
 import com.android.build.api.dsl.VariantDimension
 import com.android.build.gradle.api.BaseVariant
