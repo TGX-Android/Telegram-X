4ce519b7dccc679ad9b27b19ee57900aed747d8e
ein <MaximAlpatski@gmail.com>
2022-06-30 10:32:36 +0400

    Added animated stickers to reaction list

---
+++ app/src/main/java/org/thunderdog/challegram/component/reaction/ReactionView.java
@@ -0,0 +1,182 @@
+package org.thunderdog.challegram.component.reaction;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Interpolator;
+import android.view.animation.OvershootInterpolator;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.drinkless.td.libcore.telegram.TdApi;
+import org.thunderdog.challegram.BaseActivity;
+import org.thunderdog.challegram.component.sticker.StickerSmallView;
+import org.thunderdog.challegram.component.sticker.TGStickerObj;
+import org.thunderdog.challegram.config.Config;
+import org.thunderdog.challegram.loader.ImageFile;
+import org.thunderdog.challegram.loader.ImageReceiver;
+import org.thunderdog.challegram.loader.gif.GifFile;
+import org.thunderdog.challegram.loader.gif.GifReceiver;
+import org.thunderdog.challegram.telegram.Tdlib;
+import org.thunderdog.challegram.tool.Screen;
+import org.thunderdog.challegram.tool.UI;
+
+import me.vkryl.android.ViewUtils;
+import me.vkryl.android.animator.FactorAnimator;
+import me.vkryl.core.lambda.CancellableRunnable;
+import me.vkryl.core.lambda.Destroyable;
+
+public class ReactionView extends View implements FactorAnimator.Target, Destroyable {
+  public static final float PADDING = 8f;
+  private static final Interpolator OVERSHOOT_INTERPOLATOR = new OvershootInterpolator(3.2f);
+
+  private final ImageReceiver imageReceiver;
+  private final GifReceiver gifReceiver;
+  private final FactorAnimator animator;
+  private @Nullable
+  TGStickerObj sticker;
+  private Path contour;
+  private Tdlib tdlib;
+
+  public ReactionView (Context context) {
+    super(context);
+    this.imageReceiver = new ImageReceiver(this, 0);
+    this.gifReceiver = new GifReceiver(this);
+    this.animator = new FactorAnimator(0, this, OVERSHOOT_INTERPOLATOR, 230l);
+  }
+
+  public void init (Tdlib tdlib) {
+    this.tdlib = tdlib;
+  }
+
+  private boolean isAnimation;
+
+  public void setSticker (@Nullable TGStickerObj sticker) {
+    this.sticker = sticker;
+    startAnimation();
+    resetStickerState();
+    ImageFile imageFile = sticker != null && !sticker.isEmpty() ? sticker.getImage() : null;
+    GifFile gifFile = sticker != null && !sticker.isEmpty() ? sticker.getPreviewAnimation() : null;
+    if ((sticker == null || sticker.isEmpty()) && imageFile != null) {
+      throw new RuntimeException("");
+    }
+    contour = sticker != null ? sticker.getContour(Math.min(imageReceiver.getWidth(), imageReceiver.getHeight())) : null;
+    imageReceiver.requestFile(imageFile);
+    if (gifFile != null) {
+      gifFile.setLooped(false);
+      gifFile.setPlayOnce(true);
+    }
+    gifReceiver.requestFile(gifFile);
+  }
+
+  public void startAnimation () {
+    this.isAnimation = sticker != null && sticker.isAnimated();
+  }
+
+  public void stopAnimation () {
+    this.isAnimation = false;
+  }
+
+  public void refreshSticker () {
+    setSticker(sticker);
+  }
+
+  public void attach () {
+    imageReceiver.attach();
+    gifReceiver.attach();
+  }
+
+  public void detach () {
+    imageReceiver.detach();
+    gifReceiver.detach();
+  }
+
+  @Override
+  public void performDestroy () {
+    imageReceiver.destroy();
+    gifReceiver.destroy();
+  }
+
+  private float factor;
+
+  private void resetStickerState () {
+    animator.forceFactor(0f, true);
+    factor = 0f;
+  }
+
+  private static final float MIN_SCALE = .82f;
+
+  @Override
+  public void onFactorChanged (int id, float factor, float fraction, FactorAnimator callee) {
+    if (this.factor != factor) {
+      this.factor = factor;
+      invalidate();
+    }
+  }
+
+  private boolean isTrending;
+
+  public void setIsTrending () {
+    isTrending = true;
+  }
+
+  private boolean isSuggestion;
+
+  public void setIsSuggestion () {
+    isSuggestion = true;
+  }
+
+  @Override
+  protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec) {
+    if (isSuggestion) {
+      super.onMeasure(MeasureSpec.makeMeasureSpec(Screen.dp(72f), MeasureSpec.EXACTLY), heightMeasureSpec);
+    } else if (isTrending) {
+      super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(Screen.smallestSide() / 5, MeasureSpec.EXACTLY));
+    } else {
+      //noinspection SuspiciousNameCombination
+      super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+    int padding = Screen.dp(PADDING);
+    int width = getMeasuredWidth();
+    int height = getMeasuredHeight();
+    imageReceiver.setBounds(padding, padding + getPaddingTop(), width - padding, height - getPaddingBottom() - padding);
+    gifReceiver.setBounds(padding, padding + getPaddingTop(), width - padding, height - getPaddingBottom() - padding);
+    contour = sticker != null ? sticker.getContour(Math.min(imageReceiver.getWidth(), imageReceiver.getHeight())) : null;
+  }
+
+  @Override
+  protected void onDraw (Canvas c) {
+    boolean saved = factor != 0f;
+    if (saved) {
+      c.save();
+      float scale = MIN_SCALE + (1f - MIN_SCALE) * (1f - factor);
+      int cx = getMeasuredWidth() / 2;
+      int cy = getPaddingTop() + (getMeasuredHeight() - getPaddingBottom() - getPaddingBottom()) / 2;
+      c.scale(scale, scale, cx, cy);
+    }
+    if (isAnimation) {
+      if (gifReceiver.needPlaceholder()) {
+        if (imageReceiver.needPlaceholder()) {
+          imageReceiver.drawPlaceholderContour(c, contour);
+        }
+        imageReceiver.draw(c);
+      }
+      gifReceiver.draw(c);
+    } else {
+      if (imageReceiver.needPlaceholder()) {
+        imageReceiver.drawPlaceholderContour(c, contour);
+      }
+      imageReceiver.draw(c);
+    }
+    if (saved) {
+      c.restore();
+    }
+  }
+
+  public @Nullable TGStickerObj getSticker () {
+    return sticker;
+  }
+}
--- app/src/main/java/org/thunderdog/challegram/ui/ReactionListController.java
+++ app/src/main/java/org/thunderdog/challegram/ui/ReactionListController.java
@@ -14,7 +14,8 @@ import org.drinkless.td.libcore.telegram.TdApi;
 import org.thunderdog.challegram.R;
 import org.thunderdog.challegram.component.attach.CustomItemAnimator;
 import org.thunderdog.challegram.component.chat.EmojiToneHelper;
-import org.thunderdog.challegram.component.chat.EmojiView;
+import org.thunderdog.challegram.component.reaction.ReactionView;
+import org.thunderdog.challegram.component.sticker.TGStickerObj;
 import org.thunderdog.challegram.config.Config;
 import org.thunderdog.challegram.navigation.ViewController;
 import org.thunderdog.challegram.support.RippleSupport;
@@ -27,6 +28,7 @@ import org.thunderdog.challegram.widget.ReactionsLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import me.vkryl.android.AnimatorUtils;
@@ -69,7 +71,20 @@ public class ReactionListController extends ViewController<ReactionsLayout> {
         manager = new LinearLayoutManager(context);
         manager.setOrientation(RecyclerView.HORIZONTAL);
         toneHelper = new EmojiToneHelper(context, getArgumentsStrict().getToneDelegate(), this);
-        adapter = new ReactionListController.ReactionAdapter(context, items, this, getArgumentsStrict().getOnReactionClick());
+        adapter = new ReactionListController.ReactionAdapter(
+            context,
+            items,
+            this,
+            getArgumentsStrict().getOnReactionClick(),
+            ((reactionView, reaction) -> {
+                tdlib.client().send(new TdApi.GetAnimatedEmoji(reaction), result -> {
+                    if (result.getConstructor() == TdApi.AnimatedEmoji.CONSTRUCTOR) {
+                        TdApi.AnimatedEmoji emoji = (TdApi.AnimatedEmoji) result;
+                        TGStickerObj tgStickerObj = new TGStickerObj(tdlib, emoji.sticker, reaction, new TdApi.StickerTypeStatic());
+                        reactionView.setSticker(tgStickerObj);
+                    }
+                });
+            }));
 
         this.useDarkMode = getArgumentsStrict().useDarkMode();
 
@@ -107,44 +122,51 @@ public class ReactionListController extends ViewController<ReactionsLayout> {
     }
 
     private static class ReactionAdapter extends RecyclerView.Adapter<ReactionListController.ItemHolder> {
-        private static final int reactionSize = Screen.dp(40f);
-        private static final int reactionPadding = Screen.dp(10f);
+        private static final int REACTION_SIZE = Screen.dp(60f);
+        private static final int REACTION_PADDING = Screen.dp(10f);
 
         private final Context context;
         private final ReactionListController parent;
         private final List<Item> items;
         private final Consumer<String> onReactionClick;
+        private final BiConsumer<ReactionView, String> loadSticker;
 
-        public ReactionAdapter (Context context, List<Item> items, ReactionListController parent, Consumer<String> onReactionClick) {
+        public ReactionAdapter (
+            Context context,
+            List<Item> items,
+            ReactionListController parent,
+            Consumer<String> onReactionClick,
+            BiConsumer<ReactionView, String> loadSticker
+            ) {
             this.context = context;
             this.parent = parent;
             this.items = items;
             this.onReactionClick = onReactionClick;
+            this.loadSticker = loadSticker;
         }
         @NonNull
         @Override
         public ItemHolder onCreateViewHolder (@NonNull ViewGroup parent, int viewType) {
-            EmojiView imageView = new EmojiView(context, this.parent.toneHelper);
-            imageView.setLayoutParams(new RecyclerView.LayoutParams(reactionSize, reactionSize));
-            imageView.setPadding(
-                reactionPadding,
-                reactionPadding,
-                reactionPadding,
-                reactionPadding
-            );
-            Views.setClickable(imageView);
-            RippleSupport.setTransparentSelector(imageView);
-            return new ReactionListController.ItemHolder(imageView);
+            ReactionView reactionView = new ReactionView(context);
+            reactionView.setLayoutParams(new RecyclerView.LayoutParams(REACTION_SIZE, REACTION_SIZE));
+            RippleSupport.setTransparentSelector(reactionView);
+            Views.setClickable(reactionView);
+            return new ReactionListController.ItemHolder(reactionView);
         }
 
         @Override
         public void onBindViewHolder (@NonNull ItemHolder holder, int position) {
             ReactionListController.Item item = items.get(position);
             holder.itemView.setId(R.id.emoji);
-            holder.itemView.setOnClickListener(view -> {
+            ReactionView reactionView = (ReactionView) holder.itemView;
+            reactionView.setOnClickListener(view -> {
                 onReactionClick.accept(item.reaction);
             });
-            ((EmojiView) holder.itemView).setEmoji(item.reaction, item.reactionColorState);
+            reactionView.setOnLongClickListener(view -> {
+                reactionView.startAnimation();
+                return true;
+            });
+            loadSticker.accept(reactionView, item.reaction);
         }
 
         @Override
