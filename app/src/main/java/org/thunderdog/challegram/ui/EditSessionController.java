8e942a867b1b0f7c057cc8ca6d04b38b775e2cd2
vkryl <6242627+vkryl@users.noreply.github.com>
2022-08-21 16:30:10 +0200

    Handle case when both `session.ip` and `session.country` are empty

--- app/src/main/java/org/thunderdog/challegram/ui/EditSessionController.java
+++ app/src/main/java/org/thunderdog/challegram/ui/EditSessionController.java
@@ -36,6 +36,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import me.vkryl.android.widget.FrameLayoutFix;
+import me.vkryl.core.StringUtils;
 import me.vkryl.core.lambda.RunnableData;
 
 @SuppressLint("NonConstantResourceId")
@@ -235,9 +236,9 @@ public class EditSessionController extends EditBaseController<EditSessionControl
     items.add(new ListItem(ListItem.TYPE_SEPARATOR_FULL));
     items.add(new ListItem(ListItem.TYPE_VALUED_SETTING_COMPACT, R.id.btn_sessionPlatform, SessionIconKt.asIcon(session), (session.platform + " " + session.systemVersion).trim(), false));
     items.add(new ListItem(ListItem.TYPE_SEPARATOR_FULL));
-    items.add(new ListItem(ListItem.TYPE_VALUED_SETTING_COMPACT, R.id.btn_sessionCountry, R.drawable.baseline_location_on_24, session.country, false));
+    items.add(new ListItem(ListItem.TYPE_VALUED_SETTING_COMPACT, R.id.btn_sessionCountry, R.drawable.baseline_location_on_24, StringUtils.isEmpty(session.country) ? Lang.getString(R.string.SessionLocationUnknown) : session.country, false));
     items.add(new ListItem(ListItem.TYPE_SEPARATOR_FULL));
-    items.add(new ListItem(ListItem.TYPE_VALUED_SETTING_COMPACT, R.id.btn_sessionIp, R.drawable.baseline_router_24, session.ip, false));
+    items.add(new ListItem(ListItem.TYPE_VALUED_SETTING_COMPACT, R.id.btn_sessionIp, R.drawable.baseline_router_24, StringUtils.isEmpty(session.ip) ? Lang.getString(R.string.SessionIpUnknown) : session.ip, false));
     items.add(new ListItem(ListItem.TYPE_SHADOW_BOTTOM));
 
     if (!session.isPasswordPending) {
