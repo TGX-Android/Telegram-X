7fac8722f544f56f877558b76fe4edf75f81ba1b
vkryl <6242627+vkryl@users.noreply.github.com>
2022-12-05 17:45:03 +0400

    Fixed #275

--- app/src/main/java/org/thunderdog/challegram/util/text/Text.java
+++ app/src/main/java/org/thunderdog/challegram/util/text/Text.java
@@ -2788,7 +2788,7 @@ public class Text implements Runnable, Emoji.CountLimiter, CounterAnimator.TextD
           startIndex = spoiler.startPartIndex;
           endIndex = spoiler.startPartIndex + spoiler.partsCount;
         } else {
-          if ((onlyClickable && !foundPart.isClickable()) || (!onlyClickable && !foundPart.isClickable() && !BitwiseUtils.getFlag(textFlags, FLAG_ALL_CLICKABLE))) {
+          if (onlyClickable && !foundPart.isClickable()) {
             cancelTouch();
             return false;
           }
