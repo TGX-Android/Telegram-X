55ecab47729d929882570ffbbf6cf5655196f64a
blankie <blankie@nixnetmail.com>
2023-02-01 09:06:35 +0000

    Fix file names of files with colons in their name being truncated (#280)

--- app/src/main/java/org/thunderdog/challegram/U.java
+++ app/src/main/java/org/thunderdog/challegram/U.java
@@ -1138,7 +1138,7 @@ public class U {
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(UI.getContext(), uri)) {
         if (isExternalStorageDocument(uri)) {
           final String docId = DocumentsContract.getDocumentId(uri);
-          final String[] split = docId.split(":");
+          final String[] split = docId.split(":", 2);
           final String type = split[0];
           if ("primary".equalsIgnoreCase(type)) {
             result = Environment.getExternalStorageDirectory() + "/" + split[1];
@@ -1149,7 +1149,7 @@ public class U {
           result = getDataColumn(UI.getContext(), contentUri, null, null);
         } else if (isMediaDocument(uri)) {
           final String docId = DocumentsContract.getDocumentId(uri);
-          final String[] split = docId.split(":");
+          final String[] split = docId.split(":", 2);
           final String type = split[0];
 
           Uri contentUri = null;
