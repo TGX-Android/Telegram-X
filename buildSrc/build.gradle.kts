fc2af1f842526dacb2a18091080d8e721f4830b1
vkryl <6242627+vkryl@users.noreply.github.com>
2023-02-17 14:46:19 +0400

    Upgrade Gradle to `7.5`, AGP to `7.4.1`, Google Serivces Plugin to `4.3.15`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -45,8 +45,8 @@ configurations.all {
 
 dependencies {
   compileOnly(gradleApi())
-  implementation("com.android.tools.build:gradle:7.3.1")
-  implementation("com.google.gms:google-services:4.3.14")
+  implementation("com.android.tools.build:gradle:7.4.1")
+  implementation("com.google.gms:google-services:4.3.15")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
   implementation("com.squareup.okhttp3:okhttp:4.9.3")
   implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
