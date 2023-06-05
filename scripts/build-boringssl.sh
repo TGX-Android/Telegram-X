a52c631737cb50f58fa5aebaec46413670070bb9
vkryl <6242627+vkryl@users.noreply.github.com>
2023-06-05 22:15:51 +0400

    Removed `build-openssl.sh` & `build-boringssl.sh`

--- scripts/build-boringssl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-# shellcheck source=set-env.sh
-source "$(dirname "$0")"/set-env.sh
-
-run-cmake-impl.sh "$THIRDPARTY_LIBRARIES/boringssl" || (echo "boringssl build failed" && exit 1)
--- scripts/build-openssl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-# shellcheck source=set-env.sh
-source "$(dirname "$0")"/set-env.sh
-
-build-openssl-impl.sh || (echo "openssl build failed" && exit 1)
