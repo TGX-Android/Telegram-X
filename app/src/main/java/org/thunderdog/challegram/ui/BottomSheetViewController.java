8ef4e04b5a74a8f0666157cfa80d8d20398aaed6
Arseny271 <56611696+Arseny271@users.noreply.github.com>
2023-07-01 04:38:02 +0400

    Update app/src/main/java/org/thunderdog/challegram/ui/BottomSheetViewController.java
    Co-authored-by: Vyacheslav <6242627+vkryl@users.noreply.github.com>

--- app/src/main/java/org/thunderdog/challegram/ui/BottomSheetViewController.java
+++ app/src/main/java/org/thunderdog/challegram/ui/BottomSheetViewController.java
@@ -4,7 +4,6 @@ import android.animation.Animator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Rect;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
