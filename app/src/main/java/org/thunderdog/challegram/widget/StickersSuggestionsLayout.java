a92c03b6b1b27e12a0df654a48dd69f5cb6a4d94
Arseny271 <56611696+Arseny271@users.noreply.github.com>
2023-10-24 18:58:18 +0400

    Update StickersSuggestionsLayout.java

--- app/src/main/java/org/thunderdog/challegram/widget/StickersSuggestionsLayout.java
+++ app/src/main/java/org/thunderdog/challegram/widget/StickersSuggestionsLayout.java
@@ -76,15 +76,17 @@ public class StickersSuggestionsLayout extends AnimatedFrameLayout implements Fa
     stickerSuggestionsView = new RecyclerView(context) {
       @Override
       public boolean onTouchEvent (MotionEvent e) {
-        View v = manager.findViewByPosition(0);
-        if (v != null && v.getLeft() > e.getX()) {
-          return false;
-        }
-        int i = manager.findLastVisibleItemPosition();
-        if (i != -1) {
-          v = manager.findViewByPosition(i);
-          if (v != null && v.getRight() < e.getX()) {
+        if (e.getAction() == MotionEvent.ACTION_DOWN) {
+          View v = manager.findViewByPosition(0);
+          if (v != null && v.getLeft() > e.getX()) {
             return false;
+          }
+          int i = manager.findLastVisibleItemPosition();
+          if (i != -1) {
+            v = manager.findViewByPosition(i);
+            if (v != null && v.getRight() < e.getX()) {
+              return false;
+            }
           }
         }
         return areStickersVisible && getAlpha() == 1f && super.onTouchEvent(e);
