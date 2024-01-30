756c33af57243a0cba2e973d8d3c51ae75d4c49a
vkryl <6242627+vkryl@users.noreply.github.com>
2024-01-30 23:34:29 +0400

    Do not force show update prompt in debug builds

--- app/src/main/java/org/thunderdog/challegram/telegram/Tdlib.java
+++ app/src/main/java/org/thunderdog/challegram/telegram/Tdlib.java
@@ -10377,7 +10377,7 @@ public class Tdlib implements TdlibProvider, Settings.SettingsChangeListener, Da
           version = document.fileName.substring(prefix.length(), i == -1 ? document.fileName.length() : i);
           if (version.matches("^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$")) {
             buildNo = StringUtils.parseInt(version.substring(version.lastIndexOf('.') + 1));
-            if (buildNo > BuildConfig.ORIGINAL_VERSION_CODE || BuildConfig.DEBUG) {
+            if (buildNo > BuildConfig.ORIGINAL_VERSION_CODE) {
               ok = true;
             }
           }
