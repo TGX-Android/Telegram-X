377a11be47c7fd7cf852df5db7259a706f833dd4
Vyacheslav <6242627+vkryl@users.noreply.github.com>
2024-03-21 00:32:45 +0400

    Update app/src/main/java/org/thunderdog/challegram/data/ContentPreview.java

--- app/src/main/java/org/thunderdog/challegram/data/ContentPreview.java
+++ app/src/main/java/org/thunderdog/challegram/data/ContentPreview.java
@@ -86,8 +86,8 @@ public class ContentPreview {
   private static final int ARG_RECURRING_PAYMENT = -3;
   private static final long ADDITIONAL_MESSAGE_UI_LOAD_TIMEOUT_MS = -1; // Always async
   private static final long ADDITIONAL_MESSAGE_LOAD_TIMEOUT_MS = 0;
-  public @Nullable Emoji emoji, parentEmoji;
-  public @StringRes int placeholderText;
+  public final @Nullable Emoji emoji, parentEmoji;
+  public final @StringRes int placeholderText;
   public final @Nullable TdApi.FormattedText formattedText;
   public final boolean isTranslatable;
   public final boolean hideAuthor;
