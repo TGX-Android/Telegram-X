adf359aaa81c4854cc1c563834916feeab6b0cbd
vkryl <6242627+vkryl@users.noreply.github.com>
2024-04-23 22:26:03 +0300

    Add call testing options for development purposes

--- app/src/main/java/org/thunderdog/challegram/voip/CallConfiguration.java
+++ app/src/main/java/org/thunderdog/challegram/voip/CallConfiguration.java
@@ -63,7 +63,19 @@ public class CallConfiguration {
     boolean enableH264Encoder,
     boolean enableH264Decoder
   ) {
-    this.state = state;
+    if (VoIP.needFilterCallServers()) {
+      this.state = new TdApi.CallStateReady(
+        state.protocol,
+        VoIP.filterCallServers(state.servers),
+        state.config,
+        state.encryptionKey,
+        state.emojis,
+        state.allowP2p,
+        state.customParameters
+      );
+    } else {
+      this.state = state;
+    }
     this.isOutgoing = isOutgoing;
 
     this.persistentStateFilePath = persistentStateFile.getAbsolutePath();
