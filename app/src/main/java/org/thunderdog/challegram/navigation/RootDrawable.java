1a937a6e09665246e50d7f62971fee9e62da4858
vkryl <6242627+vkryl@users.noreply.github.com>
2025-08-29 18:56:34 +0300

    Use black color for insets

--- app/src/main/java/org/thunderdog/challegram/navigation/RootDrawable.java
+++ app/src/main/java/org/thunderdog/challegram/navigation/RootDrawable.java
@@ -15,7 +15,9 @@
 package org.thunderdog.challegram.navigation;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
@@ -55,22 +57,45 @@ public class RootDrawable extends Drawable {
     if (!state.isDisabled) {
       c.drawColor(Theme.getColor(state.getColorId()));
     }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && !Config.ENABLE_EDGE_TO_EDGE) {
-      int height = Screen.getNavigationBarHeight();
-      if (height > 0 && state.context.hadSoftwareKeysOnActivityLaunch()) {
-        int rotation = state.context.getWindowRotationDegrees();
-        Rect bounds = getBounds();
-        switch (rotation) {
-          case 0:
-          case 180:
-            c.drawRect(bounds.left, bounds.bottom - height, bounds.right, bounds.bottom, Paints.fillingPaint(UI.NAVIGATION_BAR_COLOR));
-            break;
-          case 90:
-            c.drawRect(bounds.right - height, bounds.top, bounds.right, bounds.bottom, Paints.fillingPaint(UI.NAVIGATION_BAR_COLOR));
-            break;
-          case 270:
-            c.drawRect(bounds.left, bounds.top, bounds.left + height, bounds.bottom, Paints.fillingPaint(UI.NAVIGATION_BAR_COLOR));
-            break;
+    if (Config.ENABLE_EDGE_TO_EDGE) {
+      int width = state.context.getRootView().getMeasuredWidth();
+      int height = state.context.getRootView().getMeasuredHeight();
+      Rect rect = state.context.getRootView().getSystemInsetsWithoutIme();
+      Paint paint = Paints.fillingPaint(Color.BLACK);
+      // top part
+      if (rect.top != 0) {
+        c.drawRect(0, 0, width, rect.top, paint);
+      }
+      // bottom part
+      if (rect.bottom != 0) {
+        c.drawRect(0, height - rect.bottom, width, height, paint);
+      }
+      // left part
+      if (rect.left != 0) {
+        c.drawRect(0, 0, rect.left, height, paint);
+      }
+      // right part
+      if (rect.right != 0) {
+        c.drawRect(width - rect.right, 0, width, height, paint);
+      }
+    } else {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        int height = Screen.getNavigationBarHeight();
+        if (height > 0 && state.context.hadSoftwareKeysOnActivityLaunch()) {
+          int rotation = state.context.getWindowRotationDegrees();
+          Rect bounds = getBounds();
+          switch (rotation) {
+            case 0:
+            case 180:
+              c.drawRect(bounds.left, bounds.bottom - height, bounds.right, bounds.bottom, Paints.fillingPaint(UI.NAVIGATION_BAR_COLOR));
+              break;
+            case 90:
+              c.drawRect(bounds.right - height, bounds.top, bounds.right, bounds.bottom, Paints.fillingPaint(UI.NAVIGATION_BAR_COLOR));
+              break;
+            case 270:
+              c.drawRect(bounds.left, bounds.top, bounds.left + height, bounds.bottom, Paints.fillingPaint(UI.NAVIGATION_BAR_COLOR));
+              break;
+          }
         }
       }
     }
