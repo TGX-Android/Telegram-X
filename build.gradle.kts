1b93e3633c96f906044e7e2d8f5397a825fa1965
vkryl <6242627+vkryl@users.noreply.github.com>
2025-11-11 14:30:03 +0400

    Upgrade AGP to `8.13.1`

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ buildscript {
   }
   dependencies {
     // Must be the same AGP version as in buildSrc/build.gradle.kts
-    classpath("com.android.tools.build:gradle:8.13.0")
+    classpath("com.android.tools.build:gradle:8.13.1")
   }
 }
