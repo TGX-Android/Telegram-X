6a6a53d64d8799621c22f75d2d4688338bacf47c
vkryl <6242627+vkryl@users.noreply.github.com>
2026-07-21 09:51:03 +0400

    Fix assertion caused by `MessageChatAddedToCommunity` and `MessageChatRemovedFromCommunity`

--- app/src/main/java/org/thunderdog/challegram/data/ContentPreview.java
+++ app/src/main/java/org/thunderdog/challegram/data/ContentPreview.java
@@ -753,6 +753,8 @@ public class ContentPreview {
       case TdApi.MessageManagedBotCreated.CONSTRUCTOR:
       case TdApi.MessagePollOptionAdded.CONSTRUCTOR:
       case TdApi.MessagePollOptionDeleted.CONSTRUCTOR:
+      case TdApi.MessageChatAddedToCommunity.CONSTRUCTOR:
+      case TdApi.MessageChatRemovedFromCommunity.CONSTRUCTOR:
         break;
 
       // Bots only. Unused
