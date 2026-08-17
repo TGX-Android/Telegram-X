5686681c40d3d4a1cee4c86f761b0fac3ec5e13b
vkryl <6242627+vkryl@users.noreply.github.com>
2026-08-17 17:28:10 +0400

    Enable V3 and V4 signing

--- buildSrc/src/main/kotlin/tgx/gradle/plugin/ModulePlugin.kt
+++ buildSrc/src/main/kotlin/tgx/gradle/plugin/ModulePlugin.kt
@@ -1,5 +1,6 @@
 package tgx.gradle.plugin
 
+import Abi
 import ApplicationConfig
 import Config
 import Sdk
@@ -15,7 +16,6 @@ import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.extra
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.the
-import tgx.gradle.disableRudimentaryVariants
 import tgx.gradle.findExtraFolders
 import tgx.gradle.getIntOrThrow
 import tgx.gradle.getOrThrow
@@ -170,6 +170,10 @@ open class ModulePlugin : Plugin<Project> {
                 config.keyAlias = keystore.keyAlias
                 config.keyPassword = keystore.keyPassword
                 config.enableV2Signing = true
+                config.enableV3Signing = true
+                if (config.name == "debug") {
+                  config.enableV4Signing = true
+                }
               }
             }
 
