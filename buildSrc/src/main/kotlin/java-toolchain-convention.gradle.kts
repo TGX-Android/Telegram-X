cea7f4735923ec642013ff6c6594a8ea6cee8d24
vkryl <6242627+vkryl@users.noreply.github.com>
2026-08-17 17:43:39 +0400

    Remove rudimentary imports

--- buildSrc/src/main/kotlin/java-toolchain-convention.gradle.kts
+++ buildSrc/src/main/kotlin/java-toolchain-convention.gradle.kts
@@ -1,9 +1,3 @@
-import org.gradle.api.plugins.JavaBasePlugin
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.withType
-
 plugins.withType<JavaBasePlugin> {
   extensions.configure<JavaPluginExtension> {
     toolchain {
